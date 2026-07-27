8f0123a9787e72b65b1bd4db22de5c8d1767cf51
kirthivasans <keerthisachin302@gmail.com>
2026-07-27 07:40:59 +0530

    Modified username, password, baseURL in config and test data files for scalability and easier accessibility

--- js-framework/tests/example.spec.js
+++ js-framework/tests/example.spec.js
@@ -1,13 +1,14 @@
 import {test, expect} from '@playwright/test';
+const testData = require('../data/testData.js');
 const LoginPage = require('../pages/loginPage');
 const InventoryPage = require('../pages/inventoryPage');
 const CartPage = require('../pages/cartPage');
 
 test('Login validation - POM', async({page}) => {
-    await page.goto("https://www.saucedemo.com");
+    await page.goto("/");
 
     const loginPage = new LoginPage(page);
-    await loginPage.login('standard_user', 'secret_sauce');
+    await loginPage.login('testData.validUser.username', 'testData.validUser.password');
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 
     const inventoryPage = new InventoryPage(page);
